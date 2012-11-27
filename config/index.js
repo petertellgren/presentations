7a430834d9eed6037560dacc4a05c56a0aa628f4
Peter Tellgren <peter.tellgren@gmail.com>
2012-11-27 14:54:13 +0200

    initial commit

---
+++ config/index.js
@@ -0,0 +1,14 @@
+var config = {
+	// store default list of presentations
+	presentations : {
+		'sass-less' : { // demo presentation
+			id: 'sass-less',  // id for each presentation, currently same as the url
+			title : 'Sass vs. LESS',
+			indexh : 0,  // initial slide horizontal index
+			indexv : 0,  // initial slide veriticlal index
+		}
+	}
+
+};
+
+exports.config = config;
